8588bae7ed3272521e7997f15c7d4f836febf8c1
61424 <61424@DESKTOP-P1BHIPQ>
2020-09-24 20:36:50 +1000

    Commiting

--- BasicExamples/src/DropDownExample.java
+++ BasicExamples/src/DropDownExample.java
@@ -1,37 +1,52 @@
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import org.openqa.selenium.support.ui.Select; 
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test; 
 
-public class DropDownExample extends Thread{
+public class DropDownExample{
 
-	public void run() {
+	@Test
+	public void selectDDvalues() throws Exception{
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Reddy Workspace\\Drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
-		driver.get("C:\\Reddy Workspace\\Selenium Test Cases\\BasicExamples\\dropDown.html");
+		driver.findElement(By.xpath("//*[text()='Create New Account']")).click();
 		try {
 			Thread.sleep(5000);
-			Select dropdown = new Select(driver.findElement(By.id("designation")));
+			Select dropdown = new Select(driver.findElement(By.id("month")));
 			System.out.println("Dropdown Element found");
-			dropdown.selectByIndex(1);
-			//dropdown.selectByValue("prog");
-			//dropdown.selectByVisibleText("Programmer ");
+			// It should return month
+			WebElement selected_value=dropdown.getFirstSelectedOption();
+			System.out.println("Before selected values is :"+selected_value.getText());
+			
+			// It will select march month
+			dropdown.selectByIndex(3);
+			WebElement selected_value1=dropdown.getFirstSelectedOption();
+			System.out.println("After selected values is :"+selected_value1.getText());
+			
+			
+			Thread.sleep(3000);
+			
+			// It will select october month
+			dropdown.selectByValue("10");
+			
+			Thread.sleep(3000);
+			
+			// It will select August month
+			dropdown.selectByVisibleText("Aug");
 			System.out.println("Completed");
 			
 			}
 		catch(Exception e) {
 			System.out.println("Exception:"+e);
 		}
-	}
 	
-	public static void main(String[] args) {
-	
-		DropDownExample dropDown = new DropDownExample();
-		dropDown.start();
-
 	}
 
 }
--- BasicExamples/src/DynamicXpathExample.java
+++ BasicExamples/src/DynamicXpathExample.java
@@ -1,20 +1,38 @@
+import java.awt.List;
+
+import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+
+
+
+//import com.sun.org.apache.bcel.internal.generic.Select;
 
 public class DynamicXpathExample extends Thread {
 
 	public void run() {
 
-		System.setProperty("webdriver.chrome.driver", "C:\\Reddy Workspace\\Drivers\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver", "C:\\Reddy Workspace\\Drivers\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
-		driver.get("http://localautomation.com/");
+		driver.get("https://www.amazon.com/");
 
 		try {
+			System.out.println("Entering in site");
+			//List<WebElement> list = driver.findElements(By.xpath("//span[@class='nav-line-2 nav-long-width']//span/descendant::span[@class='nav-icon nav-arrow']"));
+			//Select dropdown = new Select(driver.findElement(By.xpath("//div[@class='tabnormal']//p[contains(text(),'Products')]")));
+			System.out.println("product field identified");
+			Thread.sleep(10000);
+			//dropdown.selectByVisibleText("Create a List"); 
+			//driver.findElement(By.xpath("//a[contains(text(),'Moxa Serial Device Server')]")).click();
+			//List<WebElement> list=driver.findElements(By.xpath(""));
 			Thread.sleep(5000);
-			//driver.
-
-			//driver.close();
+		
 		}
 
 		catch (Exception e) {
